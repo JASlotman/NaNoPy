c5db412fb2c83927b293252e5d2c8d564ab37915
JASlotman <J.slotman@erasmusmc.nl>
2024-07-26 10:43:46 +0200

    Updated test file for two window test

--- NaNoPy/test.py
+++ NaNoPy/test.py
@@ -4,15 +4,24 @@ from NaNoPy import *
 xSize = 800
 ySize = 400
 
-screen = canvas(xSize,ySize)
-pen = pen(screen)
+screen = canvas(xSize,ySize,xpos=50,ypos=50)
+screen2 = canvas(xSize,ySize,xpos=50+xSize,ypos=50)
+pen1 = pen(screen)
+pen2 = pen(screen2)
 
 while screen.running:
-    pen.drawCircle(100,100,20,color().green,True)
+    pen1.drawCircle(100,100,20,color().green,True)
     screen.update()
     screen.pause(500)
     screen.clear()
     screen.update()
     screen.pause(500)
+    pen2.drawCircle(100, 100, 20, color().green, True)
+    screen2.update()
+    screen2.pause(500)
+    screen2.clear()
+    screen2.update()
+    screen2.pause(500)
 
 screen.keepWindow()
+screen2.keepWindow()
