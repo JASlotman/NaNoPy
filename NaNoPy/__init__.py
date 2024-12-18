336c81fd55c76ad4dd35c00840508c1b31b8e52e
JASlotman <J.slotman@erasmusmc.nl>
2024-12-18 12:41:59 +0100

    Changed so package works with * import, i.e. moved nanopy.py to __init__

---
+++ NaNoPy/__init__.py
@@ -0,0 +1,167 @@
+from sdl2 import *
+from sdl2.sdlgfx import *
+from sdl2.ext import *
+import ctypes
+import math
+
+
+class Mainloop:
+    def __init__(self):
+        SDL_Init(SDL_INIT_VIDEO)
+        self.event = SDL_Event()
+        self.running = True
+        self.windowlist = dict()
+
+
+    def addwindow(self,name,window_):
+        self.windowlist[name] = window_
+
+    def update(self,name):
+        window = self.windowlist.get(name)
+        SDL_ShowWindow(window)
+        ren = SDL_GetRenderer(window)
+        SDL_RenderPresent(ren)
+
+        while SDL_PollEvent(ctypes.byref(self.event)) != 0:
+            if self.event.type == SDL_WINDOWEVENT and self.event.window.event == SDL_WINDOWEVENT_CLOSE:
+                self.stop()
+
+
+    def clear(self,name):
+        ren = SDL_GetRenderer(self.windowlist.get(name))
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, 255)
+        SDL_RenderClear(ren)
+        #SDL_RenderPresent(ren)
+
+    def pause(self,name,time):
+        SDL_Delay(time)
+
+    def stop(self):
+        self.running = False
+        for win in self.windowlist:
+            SDL_DestroyWindow(self.windowlist[win])
+        SDL_Quit()
+
+    def keep(self):
+        while self.running:
+            while SDL_PollEvent(ctypes.byref(self.event)) != 0:
+                if self.event.type == SDL_WINDOWEVENT and self.event.window.event == SDL_WINDOWEVENT_CLOSE:
+                    self.stop()
+
+
+NNP = Mainloop()
+
+class canvas:
+    def __init__(self,name,xSize,ySize,*,xpos=-1,ypos=-1):
+
+        if (xpos < 0 or ypos < 0):
+            window = SDL_CreateWindow(b"NaNoPy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, xSize, ySize,SDL_WINDOW_HIDDEN)
+        else:
+            window = SDL_CreateWindow(b"NaNoPy", xpos, ypos, xSize, ySize, SDL_WINDOW_HIDDEN)
+
+        self.name = name
+        NNP.addwindow(name, window)
+
+
+    def update(self):
+        NNP.update(self.name)
+
+    def clear(self):
+        NNP.clear(self.name)
+
+    def pause(self,time):
+        NNP.pause(self.name,time)
+
+    def keepwindow(self):
+        NNP.keep()
+
+    def running(self):
+        return NNP.running
+
+
+
+
+
+
+
+
+class pen:
+    def __init__(self,window):
+        render_flags = (
+                SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+        )
+        self.renderer = SDL_CreateRenderer(NNP.windowlist.get(window.name), -1, render_flags)
+        x = list()
+        y = list()
+        x.append(0)
+        y.append(0)
+        xobj = (ctypes.c_int32 * len(x))(*x)
+        yobj = (ctypes.c_int32 * len(y))(*y)
+        SDL_GetWindowSize(NNP.windowlist.get(window.name),xobj,yobj)
+        self.xSize = xobj[0]
+        self.ySize = yobj[0]
+
+
+
+    def drawPixel(self,x,y,color):
+        pixelColor(self.renderer,int(x),int(self.ySize-y),color)
+
+    def drawLine(self,x1,y1,x2,y2,color,GFX):
+        if GFX:
+            aalineColor(self.renderer, int(x1), int(self.ySize-y1), int(x2), int(self.ySize-y2), color)
+        else:
+            SDL_SetRenderDrawBlendMode(self.renderer, SDL_BLENDMODE_NONE)
+            SDL_SetRenderDrawColor(self.renderer, color.a,color.b,color.g,color.r)
+            SDL_RenderDrawLine(self.renderer, int(x1), int(self.ySize-y1), int(x2), int(self.ySize-y2))
+
+    def drawThickLine(self,x1,y1,x2,y2,w,color):
+        thickLineColor(self.renderer, int(x1), int(self.ySize-y1), int(x2), int(self.ySize-y2),int(w), color)
+
+    def drawRectangle(self,x1,y1,w,h,color,filled):
+        if filled:
+            boxColor(self.renderer,int(x1),int(self.ySize-y1),int(x1+w),int((self.ySize-y1)+h),color)
+        else:
+            rectangleColor(self.renderer, int(x1), int(y1), int(x1+w),int((self.ySize-y1)+h), color)
+
+    def drawCircle(self,x,y,r,color,filled):
+        if filled:
+            filledCircleColor(self.renderer,int(x),int(self.ySize-y),int(r),color)
+        else:
+            aacircleColor(self.renderer,int(x),int(self.ySize-y),int(r),color)
+
+    def drawStar(self,x,y,r,n,color, filled):
+        rads = (2*math.pi)/(2*n)
+        xs = list()
+        ys = list()
+
+        for i in range(n*2):
+            rad = r / ( (i % 2) + 1 )
+            xs.append(int(x+math.cos(rads*i)*rad))
+            ys.append(int((self.ySize)-y+math.sin(rads*i)*rad))
+
+        vx = (ctypes.c_int16 * len(xs))(*xs)
+        vy = (ctypes.c_int16 * len(ys))(*ys)
+
+        if filled:
+            filledPolygonColor(self.renderer, vx, vy, n*2, color)
+        else:
+            aapolygonColor(self.renderer,vx,vy,n*2,color)
+
+    def drawString(self,x,y,color,text):
+        gfxPrimitivesSetFont(None, 0, 0)
+        stringColor(self.renderer, int(x), int(self.ySize - y), str.encode(text), color)
+
+class color:
+    def __init__(self):
+        self.red = Color(255,0,0,255)
+        self.blue = Color(255, 255, 0, 0)
+        self.green = Color(255, 0, 255,0 )
+        self.yellow = Color(255, 0, 255, 255)
+        self.magenta = Color(255, 255, 0, 255)
+        self.cyan = Color(255, 255, 255,0 )
+        self.white = Color(255,255,255,255)
+        self.gray = Color(255,155,155,155)
+
+
+    def __call__(self,*,r=255,g=0,b=0):
+        return Color(255,b,g,r)
