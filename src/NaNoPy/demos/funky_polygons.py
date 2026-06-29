2bf4ca9b7c6ffe1591299e45d01124058de99870
LEANDER LUTZE <L.A.Lutze@tudelft.nl>
2026-06-29 14:18:08 +0200

    added demo for new polygon drawing method

---
+++ src/NaNoPy/demos/funky_polygons.py
@@ -0,0 +1,16 @@
+from NaNoPy import Canvas, Writer, Color
+from time import sleep
+
+def demo() -> None:
+    screen = Canvas("funky_polygons.py", 400, 400)
+    pen = Writer(screen)
+
+    pen.draw_polygon_custom([(10, 30), (500, 500), (100, 0)], Color.white, True)
+    pen.draw_star(100, 100, 20, 5, Color.white, True)
+    pen.draw_polygon_regular(20, 20, 20, 6, Color.white, filled=True)
+
+    screen.update()
+    sleep(3)
+
+if __name__ == "__main__":
+    demo()
